0df1ee2610ebc5d0f730f40385a925802b0bc574
German Espitia <ge.espitia@gmail.com>
2016-01-13 10:10:10 -0500

    Add SignupVC UI and files

---
+++ Tribes/SignupViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignupViewController.h
+//  Tribes
+//
+//  Created by German Espitia on 1/13/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignupViewController : UIViewController
+
+@end
---
+++ Tribes/SignupViewController.m
@@ -0,0 +1,39 @@
+//
+//  SignupViewController.m
+//  Tribes
+//
+//  Created by German Espitia on 1/13/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import "SignupViewController.h"
+#import <DigitsKit/DigitsKit.h>
+
+
+@interface SignupViewController ()
+
+@end
+
+@implementation SignupViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
