8f2d097b5e9a4edb86f8ad3a0ea9c72992688b2b
German Espitia <ge.espitia@gmail.com>
2016-01-14 10:12:31 -0500

    Add Tribe model class files

---
+++ Tribes/TribeHandler.h
@@ -0,0 +1,15 @@
+//
+//  TribeHandler.h
+//  Tribes
+//
+//  Created by German Espitia on 1/14/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Parse.h"
+
+@interface TribeHandler : NSObject
+
+
+@end
---
+++ Tribes/TribeHandler.m
@@ -0,0 +1,14 @@
+//
+//  TribeHandler.m
+//  Tribes
+//
+//  Created by German Espitia on 1/14/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import "TribeHandler.h"
+
+@implementation TribeHandler
+
+
+@end
