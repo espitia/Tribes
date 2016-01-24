d66d0457a1a118417170fd7a1d5b5de9e8b65e65
German Espitia <ge.espitia@gmail.com>
2016-01-24 14:41:51 -0500

    Add function to update completions # by counting completionDates

--- Tribes/Activity.m
+++ Tribes/Activity.m
@@ -29,6 +29,7 @@
 
 -(void)completeForToday {
     [self addObject:[NSDate date] forKey:@"completionDates"];
+    [self updateCompletions];
     [self saveInBackground];
 }
 
@@ -43,8 +44,13 @@
     // if it doesn't exist, it is a new tribe activity
     return false;
 }
-
+/**
+* Check if date is today
+* @param Date to be checked
+* @return BOOL with whether or not date is today
+*/
 -(BOOL)isToday:(NSDate *)date {
+    
     NSCalendar *cal = [NSCalendar currentCalendar];
     NSDateComponents *components = [cal components:(NSCalendarUnitEra | NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay) fromDate:[NSDate date]];
     NSDate *today = [cal dateFromComponents:components];
@@ -52,5 +58,11 @@
     NSDate * dateToCheck = [cal dateFromComponents:components];
     
     return ([today isEqualToDate:dateToCheck]) ? true : false;
+}
+/**
+ * Update completions by counting total dates in completion dates
+ */
+-(void)updateCompletions {
+    self[@"completions"] = [NSNumber numberWithInteger:self.completionDates.count];
 }
 @end
