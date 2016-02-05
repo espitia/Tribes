611b1d77065915fb3d1814e2d26e6eaf6ff33700
German Espitia <germanespitia@germans-mbp.home>
2016-02-05 13:03:09 -0500

    - Add alert method to show alert. - Show alert when user completes activity letting him know he received xp

--- Tribes/TribesTableViewController.m
+++ Tribes/TribesTableViewController.m
@@ -145,6 +145,7 @@
     [cell setSwipeGestureWithView:checkView color:greenColor mode:MCSwipeTableViewCellModeSwitch state:MCSwipeTableViewCellState1 completionBlock:^(MCSwipeTableViewCell *cell, MCSwipeTableViewCellState state, MCSwipeTableViewCellMode mode) {
 
         [currentUser completeActivityForTribe:tribe];
+        [self showAlertWithTitle:@"🔑🔑🔑" andMessage:@"+ 100xp"];
         [self.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
     }];
 }
@@ -228,6 +229,38 @@
     }];
 }
 
+#pragma mark - Alerts
+
+-(void)showAlertWithTitle:(NSString *)title andMessage:(NSString *)message {
+    
+    // weak self to not have any issues to present alert view
+    __unsafe_unretained typeof(self) weakSelf = self;
+    
+    // alert controller
+    UIAlertController * __block alert;
+    UIAlertAction * __block defaultAction;
+    
+    // message to go in alert view
+    NSString * __block alertTitle = title;
+    NSString * __block alertMessage = message;
+    
+    defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                                           handler:^(UIAlertAction * action) {
+                                               
+                                           }];
+    
+    // finish alert set up
+    alert = [UIAlertController alertControllerWithTitle:alertTitle
+                                                message:alertMessage
+                                         preferredStyle:UIAlertControllerStyleAlert];
+    
+    
+    // add action (if success, pop to tribe VC)
+    [alert addAction:defaultAction];
+    
+    // present alert
+    [weakSelf presentViewController:alert animated:YES completion:nil];
+}
 @end
 
 
