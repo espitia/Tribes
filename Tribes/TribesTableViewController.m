06814ef207369c845807293ed164ed43d1ef9c62
German Espitia <ge.espitia@gmail.com>
2016-02-09 22:58:35 -0500

    Add progress bar to see how far user is from fully doing all activities

--- Tribes/TribesTableViewController.m
+++ Tribes/TribesTableViewController.m
@@ -14,10 +14,12 @@
 #import "MCSwipeTableViewCell.h"
 #import "User.h"
 #import "Activity.h"
+#import "YLProgressBar.h"
 
 @interface TribesTableViewController () <MCSwipeTableViewCellDelegate> {
     User * currentUser;
     UIRefreshControl * refreshControl;
+    YLProgressBar * progressBar;
 }
 
 @end
@@ -44,6 +46,9 @@
         [currentUser loadTribesWithBlock:^{
             self.navigationItem.title = [NSString stringWithFormat:@"lvl %d - %dxp", currentUser.lvl, currentUser.xp];
             [self.tableView reloadData];
+            
+            // add and update progress bar
+            [self addProgressBar];
         }];
     }
 
@@ -145,6 +150,7 @@
     [cell setSwipeGestureWithView:checkView color:greenColor mode:MCSwipeTableViewCellModeSwitch state:MCSwipeTableViewCellState1 completionBlock:^(MCSwipeTableViewCell *cell, MCSwipeTableViewCellState state, MCSwipeTableViewCellMode mode) {
 
         [currentUser completeActivityForTribe:tribe];
+        [self updateProgressBar];
         [self showAlertWithTitle:@"🔑🔑🔑" andMessage:@"+ 100xp"];
         [self.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
     }];
@@ -261,6 +267,57 @@
     // present alert
     [weakSelf presentViewController:alert animated:YES completion:nil];
 }
+
+-(void)addProgressBar {
+
+    // colors
+    NSArray *rainbowColors = @[[UIColor colorWithRed:33/255.0f green:180/255.0f blue:162/255.0f alpha:1.0f],
+                               [UIColor colorWithRed:3/255.0f green:137/255.0f blue:166/255.0f alpha:1.0f],
+                               [UIColor colorWithRed:91/255.0f green:63/255.0f blue:150/255.0f alpha:1.0f],
+                               [UIColor colorWithRed:87/255.0f green:26/255.0f blue:70/255.0f alpha:1.0f],
+                               [UIColor colorWithRed:126/255.0f green:26/255.0f blue:36/255.0f alpha:1.0f],
+                               [UIColor colorWithRed:149/255.0f green:37/255.0f blue:36/255.0f alpha:1.0f],
+                               [UIColor colorWithRed:228/255.0f green:69/255.0f blue:39/255.0f alpha:1.0f],
+                               [UIColor colorWithRed:245/255.0f green:166/255.0f blue:35/255.0f alpha:1.0f],
+                               [UIColor colorWithRed:165/255.0f green:202/255.0f blue:60/255.0f alpha:1.0f],
+                               [UIColor colorWithRed:202/255.0f green:217/255.0f blue:54/255.0f alpha:1.0f],
+                               [UIColor colorWithRed:111/255.0f green:188/255.0f blue:84/255.0f alpha:1.0f]];
+    
+    progressBar = [[YLProgressBar alloc] initWithFrame:CGRectMake(0,0,self.tableView.frame.size.width,5)];
+    progressBar.type                = YLProgressBarTypeFlat;
+    progressBar.trackTintColor      = [UIColor clearColor];
+    progressBar.progressTintColors   = rainbowColors;
+    progressBar.hideGloss           = YES;
+    progressBar.hideStripes         = YES;
+    progressBar.indicatorTextDisplayMode = YLProgressBarIndicatorTextDisplayModeTrack;
+    [self.tableView addSubview:progressBar];
+    
+    [self updateProgressBar];
+    
+}
+-(void)updateProgressBar {
+    
+    float numberOfTribes = (float)currentUser.tribes.count;
+    float completionProgress = 100/numberOfTribes;
+    float completions;
+    for (Activity * activity in currentUser.activities) {
+        if ([activity completedForDay]) {
+            completions++;
+        }
+    }
+    float totalProgress = (completionProgress * completions) * .01 ;
+    [progressBar setProgress:totalProgress animated:YES];
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    
+    // add offset to progress bar frame so it doesn't move
+    CGRect frame = progressBar.frame;
+    frame.origin.y = scrollView.contentOffset.y + 64;
+    [progressBar setFrame:frame];
+    [self.view bringSubviewToFront:progressBar];
+}
+
 @end
 
 
