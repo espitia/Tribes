9b43c7b1edea9bd442a2c4352e61200aebde730e
German Espitia <ge.espitia@gmail.com>
2016-02-10 12:49:45 -0500

    Add loading signifier to let the user know tribes are loading

--- Tribes/TribesTableViewController.m
+++ Tribes/TribesTableViewController.m
@@ -42,6 +42,9 @@
     if (!currentUser) {
         [self signUp];
     } else {
+        
+        self.navigationItem.title = @"Loading Tribes..";
+        
         [currentUser loadTribesWithBlock:^{
             //            self.navigationItem.title = [NSString stringWithFormat:@"lvl %d - %dxp", currentUser.lvl, currentUser.xp];
             self.navigationItem.title = @"Tribes";
