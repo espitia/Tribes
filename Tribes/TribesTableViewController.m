cce4d906eb1e33a39988953b1e1c06afa9b4109d
German Espitia <ge.espitia@gmail.com>
2016-02-12 16:10:41 -0500

    Change rainbow color progress bar to green.

--- Tribes/TribesTableViewController.m
+++ Tribes/TribesTableViewController.m
@@ -300,24 +300,12 @@
 }
 
 -(void)addProgressBar {
-
-    // colors
-    NSArray *rainbowColors = @[[UIColor colorWithRed:33/255.0f green:180/255.0f blue:162/255.0f alpha:1.0f],
-                               [UIColor colorWithRed:3/255.0f green:137/255.0f blue:166/255.0f alpha:1.0f],
-                               [UIColor colorWithRed:91/255.0f green:63/255.0f blue:150/255.0f alpha:1.0f],
-                               [UIColor colorWithRed:87/255.0f green:26/255.0f blue:70/255.0f alpha:1.0f],
-                               [UIColor colorWithRed:126/255.0f green:26/255.0f blue:36/255.0f alpha:1.0f],
-                               [UIColor colorWithRed:149/255.0f green:37/255.0f blue:36/255.0f alpha:1.0f],
-                               [UIColor colorWithRed:228/255.0f green:69/255.0f blue:39/255.0f alpha:1.0f],
-                               [UIColor colorWithRed:245/255.0f green:166/255.0f blue:35/255.0f alpha:1.0f],
-                               [UIColor colorWithRed:165/255.0f green:202/255.0f blue:60/255.0f alpha:1.0f],
-                               [UIColor colorWithRed:202/255.0f green:217/255.0f blue:54/255.0f alpha:1.0f],
-                               [UIColor colorWithRed:111/255.0f green:188/255.0f blue:84/255.0f alpha:1.0f]];
     
+    // set up progr
     progressBar = [[YLProgressBar alloc] initWithFrame:CGRectMake(0,0,self.tableView.frame.size.width,5)];
     progressBar.type                = YLProgressBarTypeFlat;
     progressBar.trackTintColor      = [UIColor clearColor];
-    progressBar.progressTintColors   = rainbowColors;
+    progressBar.progressTintColor   = [UIColor greenColor];
     progressBar.hideGloss           = YES;
     progressBar.hideStripes         = YES;
     progressBar.indicatorTextDisplayMode = YLProgressBarIndicatorTextDisplayModeTrack;
@@ -329,7 +317,7 @@
 -(void)updateProgressBar {
     
     float numberOfTribes = (float)currentUser.tribes.count;
-    float completionProgress = 100/numberOfTribes;
+    float completionProgress = 100.0/numberOfTribes;
     float completions;
     for (Activity * activity in currentUser.activities) {
         if ([activity completedForDay]) {
