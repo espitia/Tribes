e55d2172e08a9ca957772adbeccb1f5c876c9f4f
German Espitia <ge.espitia@gmail.com>
2016-02-13 14:38:46 -0500

    username and password validation (basic): - username is not taken - password is 'secure' - password is confirmed

--- Tribes/SignupViewController.m
+++ Tribes/SignupViewController.m
@@ -32,9 +32,98 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)signUp:(id)sender {
+#pragma mark Text Field Delegate
+
+-(void)textFieldDidEndEditing:(UITextField *)textField {
+   
+    // HANDLE USERNAME
+    if (textField == username) {
+        
+        PFQuery * query = [PFUser query];
+        [query whereKey:@"username" equalTo:textField.text];
+        [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
+            if (!error) {
+                if (objects.count > 0) {
+                    [self showAlertWithTitle:@"❌❌❌" andMessage:@"Username is taken! Please try another one."];
+                } else {
+                    NSLog(@"username NOT TAKEN");
+                }
+            }
+        }];
+        
+    }
+    // HANDLE FIRST PASSWORD FIELD
+    else if (textField == password) {
+        
+        if ([self isValidPassword:password.text]) {
+            NSLog(@"password valid");
+        } else {
+            [self showAlertWithTitle:@"❌❌❌" andMessage:@"Pass word must be 8 characters long and include at least 1 letter and 1 number."];
+        }
+        
+        
+    }
+    // HANDLE SECOND PASSWORD FIELD
+    else if (textField == confirmPassword) {
+        
+        if ([textField.text isEqualToString:password.text]) {
+            NSLog(@"second pass valid");
+        } else {
+            [self showAlertWithTitle:@"❌❌❌" andMessage:@"Passwords don't match!"];
+        }
+        
+    }
+}
+
+#pragma mark - Util
+// *** Validation for Password ***
+
+// "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" --> (Minimum 8 characters at least 1 Alphabet and 1 Number)
+// "^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$" --> (Minimum 8 and Maximum 16 characters at least 1 Alphabet, 1 Number and 1 Special Character)
+// "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$" --> (Minimum 8 characters at least 1 Uppercase Alphabet, 1 Lowercase Alphabet and 1 Number)
+// "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}" --> (Minimum 8 characters at least 1 Uppercase Alphabet, 1 Lowercase Alphabet, 1 Number and 1 Special Character)
+// "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,10}" --> (Minimum 8 and Maximum 10 characters at least 1 Uppercase Alphabet, 1 Lowercase Alphabet, 1 Number and 1 Special Character)
+
+-(BOOL)isValidPassword:(NSString *)passwordString {
+    NSString *stricterFilterString = @"^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\fd22\\d]{8,}$";
+    NSPredicate *passwordTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", stricterFilterString];
+    return [passwordTest evaluateWithObject:passwordString];
+}
+
+-(void)showAlertWithTitle:(NSString *)title andMessage:(NSString *)message {
     
-    NSLog(@"%@", [Digits sharedInstance].session);
+    // weak self to not have any issues to present alert view
+    __unsafe_unretained typeof(self) weakSelf = self;
+    
+    // alert controller
+    UIAlertController * __block alert;
+    UIAlertAction * __block defaultAction;
+    
+    // message to go in alert view
+    NSString * __block alertTitle = title;
+    NSString * __block alertMessage = message;
+    
+    defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                                           handler:^(UIAlertAction * action) {
+                                               
+                                           }];
+    
+    // finish alert set up
+    alert = [UIAlertController alertControllerWithTitle:alertTitle
+                                                message:alertMessage
+                                         preferredStyle:UIAlertControllerStyleAlert];
+    
+    
+    // add action (if success, pop to tribe VC)
+    [alert addAction:defaultAction];
+    
+    // present alert
+    [weakSelf presentViewController:alert animated:YES completion:nil];
+}
+
+#pragma mark - Sign Up User
+
+- (IBAction)signUp:(id)sender {
     
     // sign up with phonenumber (digits by twitter Fabrics)
     [[Digits sharedInstance] authenticateWithCompletion:^(DGTSession *session, NSError *error) {
