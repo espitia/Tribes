0f957f6fecc03b6f466089b1624320f991e702bc
German Espitia <ge.espitia@gmail.com>
2016-02-18 16:15:35 -0500

    Add ExplainerToPhoneVerificationViewController initial view and files. This is to show why we need the phone verification

---
+++ Tribes/ExplainerToPhoneVerificationViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExplainerToPhoneVerificationViewController.h
+//  Tribes
+//
+//  Created by German Espitia on 2/13/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExplainerToPhoneVerificationViewController : UIViewController
+
+@end
---
+++ Tribes/ExplainerToPhoneVerificationViewController.m
@@ -0,0 +1,40 @@
+//
+//  ExplainerToPhoneVerificationViewController.m
+//  Tribes
+//
+//  Created by German Espitia on 2/13/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import "ExplainerToPhoneVerificationViewController.h"
+
+@interface ExplainerToPhoneVerificationViewController ()
+
+@end
+
+@implementation ExplainerToPhoneVerificationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)verifyPhoneNumber:(id)sender {
+    NSLog(@"worked");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
