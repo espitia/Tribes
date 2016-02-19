79309f77248ee8118bd6b8c4fe19f779940416a4
German Espitia <ge.espitia@gmail.com>
2016-02-18 19:16:59 -0500

    Add methods to make it rain confetti!

--- Tribes/TribesTableViewController.m
+++ Tribes/TribesTableViewController.m
@@ -14,12 +14,14 @@
 #import "MCSwipeTableViewCell.h"
 #import "User.h"
 #import "Activity.h"
+#import "KRConfettiView.h"
 #import "YLProgressBar.h"
 
 @interface TribesTableViewController () <MCSwipeTableViewCellDelegate> {
     User * currentUser;
     UIRefreshControl * refreshControl;
     YLProgressBar * progressBar;
+    KRConfettiView * confettiView;
 }
 
 @end
@@ -338,6 +340,49 @@
     [self.view bringSubviewToFront:progressBar];
 }
 
+
+#pragma mark - Confetti
+
+-(void)makeItRainConfettiForTime:(float)time {
+    
+    // if confetti has not been initialized
+    if (!confettiView) {
+        
+        // Create confetti view
+        confettiView = [[KRConfettiView alloc] initWithFrame:self.view.frame];
+        
+        // Set colors (default colors are red, green and blue)
+        confettiView.colours = @[[UIColor colorWithRed:0.95 green:0.40 blue:0.27 alpha:1.0],
+                                 [UIColor colorWithRed:1.00 green:0.78 blue:0.36 alpha:1.0],
+                                 [UIColor colorWithRed:0.48 green:0.78 blue:0.64 alpha:1.0],
+                                 [UIColor colorWithRed:0.30 green:0.76 blue:0.85 alpha:1.0],
+                                 [UIColor colorWithRed:0.58 green:0.39 blue:0.55 alpha:1.0]];
+        
+        //Set intensity (from 0 - 1, default intensity is 0.5)
+        confettiView.intensity = 0.7;
+        
+        //set type
+        confettiView.type = Diamond;
+        
+        //For custom image
+        //confettiView.customImage = [UIImage imageNamed:@"diamond"];
+        //confettiView.type = Custom;
+    }
+
+    [NSTimer scheduledTimerWithTimeInterval:0.7f
+                                     target:self
+                                   selector: @selector(stopConfetti)
+                                   userInfo:nil
+                                    repeats:NO];
+    //add subview
+    [self.view addSubview:confettiView];
+    
+    // make it rain confetti!
+    [confettiView startConfetti];
+}
+-(void)stopConfetti {
+    [confettiView stopConfetti];
+}
 @end
 
 
