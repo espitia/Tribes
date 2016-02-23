98c5da0190ec0d589e76d241311b47f3433ea1fd
German Espitia <ge.espitia@gmail.com>
2016-02-22 20:18:01 -0500

    check if duetime is nil and set editingDueTime BOOL (which open timepicker) correctly.

--- Tribes/SettingsTableViewController.m
+++ Tribes/SettingsTableViewController.m
@@ -21,7 +21,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title = @"Settings 🔧";
-    editingDueTime = _activity.dueTime;
+    editingDueTime = (_activity.dueTime) ? true : false;
 }
 
 
