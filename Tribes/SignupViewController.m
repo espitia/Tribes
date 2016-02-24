66d33ec38cd314b8a32b10d92eafee8b464e0154
German Espitia <ge.espitia@gmail.com>
2016-02-24 13:46:31 -0500

    Finishing add functionality for sign up. couldn't commit correctly because in the end, all was jumbled up. lesson learned.

--- Tribes/SignupViewController.m
+++ Tribes/SignupViewController.m
@@ -63,117 +63,101 @@
     [email becomeFirstResponder];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 #pragma mark Text Field Delegate
 
--(void)textFieldDidEndEditing:(UITextField *)textField {
-   
-    // HANDLE USERNAME
-    if (textField == username) {
-        
-        PFQuery * query = [PFUser query];
-        [query whereKey:@"username" equalTo:textField.text];
-        [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
-            if (!error) {
-                if (objects.count > 0) {
-                    [self showAlertWithTitle:@"❌❌❌" andMessage:@"Username is taken! Please try another one."];
-                } else {
-                    NSLog(@"username NOT TAKEN");
-                }
-            }
-        }];
-        
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+//    textField.rightView = nil;
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    
+    NSString * completeString;
+
+    if ([string isEqualToString:@""]) {
+        completeString = [textField.text substringToIndex:[textField.text length]-1];
+    } else {
+        completeString = [textField.text stringByAppendingString:string];
     }
-    // HANDLE FIRST PASSWORD FIELD
+    
+    [self validateTextField:textField withInput:completeString];
+    
+    return true;
+    
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if (textField == email) {
+        [textField resignFirstResponder];
+        [username becomeFirstResponder];
+    } else if (textField == username) {
+        [textField resignFirstResponder];
+        [password becomeFirstResponder];
+    }
+    return YES;
+}
+
+-(void)textFieldDidEndEditing:(UITextField *)textField {
+    [self validateTextField:textField withInput:textField.text];
+}
+
+#pragma mark - Right View Touch Delegate
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+
+}
+
+
+#pragma mark - Validation
+
+-(void)validateTextField:(UITextField *)textField withInput:(NSString *)completeString {
+    
+    __block UIImage * imgSignifier;
+    
+    if (textField == email) {
+        
+        if ([validation isEmailValid:completeString]) {
+            imgSignifier = [UIImage imageNamed:@"green-check"];
+            emailValid = true;
+        } else {
+            imgSignifier = [UIImage imageNamed:@"red-cross"];
+            emailValid = false;
+        }
+    }
+    
+    else if (textField == username) {
+       
+        
+        [validation isUsernameValid:completeString withBlock:^(BOOL success) {
+            if (success) {
+                imgSignifier = [UIImage imageNamed:@"green-check"];
+                usernameValid = true;
+            } else {
+                imgSignifier = [UIImage imageNamed:@"red-cross"];
+                usernameValid = false;
+            }
+            [self setSignifierForTextField:textField withImage:imgSignifier];
+            [self isReadyToSignUp];
+        }];
+
+    }
+    
     else if (textField == password) {
         
-        if ([self isValidPassword:password.text]) {
-            NSLog(@"password valid");
+        if ([validation isPasswordValid:completeString]) {
+            imgSignifier = [UIImage imageNamed:@"green-check"];
+            passwordValid = true;
         } else {
-            [self showAlertWithTitle:@"❌❌❌" andMessage:@"Pass word must be 8 characters long and include at least 1 letter and 1 number."];
+            imgSignifier = [UIImage imageNamed:@"red-cross"];
+            passwordValid = false;
         }
-        
-        
     }
-    // HANDLE SECOND PASSWORD FIELD
-    else if (textField == confirmPassword) {
-        
-        if ([textField.text isEqualToString:password.text]) {
-            NSLog(@"second pass valid");
-        } else {
-            [self showAlertWithTitle:@"❌❌❌" andMessage:@"Passwords don't match!"];
-        }
-        
-    }
-}
 
-#pragma mark - Util
-// *** Validation for Password ***
+    
+    [self setSignifierForTextField:textField withImage:imgSignifier];
+    
+    [self isReadyToSignUp];
 
-// "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" --> (Minimum 8 characters at least 1 Alphabet and 1 Number)
-// "^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$" --> (Minimum 8 and Maximum 16 characters at least 1 Alphabet, 1 Number and 1 Special Character)
-// "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$" --> (Minimum 8 characters at least 1 Uppercase Alphabet, 1 Lowercase Alphabet and 1 Number)
-// "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}" --> (Minimum 8 characters at least 1 Uppercase Alphabet, 1 Lowercase Alphabet, 1 Number and 1 Special Character)
-// "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,10}" --> (Minimum 8 and Maximum 10 characters at least 1 Uppercase Alphabet, 1 Lowercase Alphabet, 1 Number and 1 Special Character)
-
--(BOOL)isValidPassword:(NSString *)passwordString {
-    NSString *stricterFilterString = @"^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\fd22\\d]{8,}$";
-    NSPredicate *passwordTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", stricterFilterString];
-    return [passwordTest evaluateWithObject:passwordString];
-}
-
--(void)showAlertWithTitle:(NSString *)title andMessage:(NSString *)message {
-    
-    // weak self to not have any issues to present alert view
-    __unsafe_unretained typeof(self) weakSelf = self;
-    
-    // alert controller
-    UIAlertController * __block alert;
-    UIAlertAction * __block defaultAction;
-    
-    // message to go in alert view
-    NSString * __block alertTitle = title;
-    NSString * __block alertMessage = message;
-    
-    defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
-                                           handler:^(UIAlertAction * action) {
-                                               
-                                           }];
-    
-    // finish alert set up
-    alert = [UIAlertController alertControllerWithTitle:alertTitle
-                                                message:alertMessage
-                                         preferredStyle:UIAlertControllerStyleAlert];
-    
-    
-    // add action (if success, pop to tribe VC)
-    [alert addAction:defaultAction];
-    
-    // present alert
-    [weakSelf presentViewController:alert animated:YES completion:nil];
-}
-
--(BOOL)isUsernameValid:(NSString *)usernameToCheck {
-    
-    __block BOOL valid;
-    
-    PFQuery * query = [PFUser query];
-    [query whereKey:@"username" equalTo:usernameToCheck];
-    [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
-        if (!error) {
-            if (objects.count > 0) {
-                valid = false;
-            } else {
-                valid = true;
-            }
-        }
-    }];
-    
-    return valid;
 }
 
 #pragma mark - Sign Up User
