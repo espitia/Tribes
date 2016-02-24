4ed9ef95e780f0e590f81f77d8b4deec9b599d1c
German Espitia <ge.espitia@gmail.com>
2016-02-24 13:38:45 -0500

    - Add main vc with signup/login. - Hide navigation bar

---
+++ Tribes/SignUpOrLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignUpOrLoginViewController.h
+//  Tribes
+//
+//  Created by German Espitia on 2/23/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignUpOrLoginViewController : UIViewController
+
+@end
