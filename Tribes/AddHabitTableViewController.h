bbdbaca92ae5845d1824407c7f6cad6814c03c97
German Espitia <ge.espitia@gmail.com>
2016-03-08 17:47:55 -0500

    Add habit UI and controller files.

---
+++ Tribes/AddHabitTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddHabitTableViewController.h
+//  Tribes
+//
+//  Created by German Espitia on 3/8/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddHabitTableViewController : UITableViewController
+
+@end
