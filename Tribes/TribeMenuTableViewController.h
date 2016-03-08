e8d3a917db65918284b5b2bf468134382eac4227
German Espitia <ge.espitia@gmail.com>
2016-03-08 11:27:41 -0500

    Add files for Tribe Menu Control (members/habits)

---
+++ Tribes/TribeMenuTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TribeMenuTableViewController.h
+//  Tribes
+//
+//  Created by German Espitia on 3/8/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TribeMenuTableViewController : UITableViewController
+
+@end
