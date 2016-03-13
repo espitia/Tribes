9b654293a051ed3f1691d04888079a2d23b2066e
German Espitia <ge.espitia@gmail.com>
2016-03-12 23:16:13 -0500

    Add UI and Files setup for FriendsVC

---
+++ Tribes/FriendsTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  FriendsTableViewController.h
+//  Tribes
+//
+//  Created by German Espitia on 3/12/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Tribe.h"
+
+@interface FriendsTableViewController : UITableViewController
+@property (nonatomic, strong) Tribe * tribe;
+
+@end
