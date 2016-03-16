60386a7721313ec6a06350ab6709581035a84f61
German Espitia <ge.espitia@gmail.com>
2016-03-16 01:53:01 -0400

    get tribestablevc and reload data to show newly added habits when moving from tribes menu to tribes table vc

--- Tribes/TribeMenuTableViewController.m
+++ Tribes/TribeMenuTableViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "TribeMenuTableViewController.h"
+#import "TribesTableViewController.h"
 #import "MembersTableViewController.h"
 #import "HabitsTableViewController.h"
 #import "User.h"
@@ -110,4 +111,17 @@
         
     }
 }
+-(void)willMoveToParentViewController:(UIViewController *)parent {
+    
+    // if moving back to tribestablevc
+    if (![parent isEqual:self.parentViewController]) {
+        
+        // get tribestablevc and reload data to show newly added habits
+        UINavigationController * navController = (UINavigationController *)self.parentViewController;
+        TribesTableViewController * tribesVc = (TribesTableViewController *)navController.viewControllers[0];
+        [tribesVc.tableView reloadData];
+    }
+
+}
+
 @end
