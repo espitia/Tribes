446ce02a867c5af4dbd23a39fc53bf78017c01a4
German Espitia <ge.espitia@gmail.com>
2016-03-17 17:17:08 -0400

    Add files for SettingsTableViewController

---
+++ Tribes/SettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.h
+//  Tribes
+//
+//  Created by German Espitia on 3/17/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTableViewController : UITableViewController
+
+@end
