f8a938341ed593ac24e436768a963b2821386f1d
German Espitia <ge.espitia@gmail.com>
2016-03-28 18:49:45 -0400

    Add weekly report UI and files

---
+++ Tribes/WeeklyReportTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  WeeklyReportTableViewController.h
+//  Tribes
+//
+//  Created by German Espitia on 3/28/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Tribe.h"
+
+@interface WeeklyReportTableViewController : UITableViewController
+
+@property (atomic, strong) Tribe * tribe;
+
+@end
