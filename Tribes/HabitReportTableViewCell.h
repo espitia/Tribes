261a1ab26f3cb04c3e2d6e30bb449cee1b9275e7
German Espitia <ge.espitia@gmail.com>
2016-03-29 13:11:10 -0400

    Add habit report custom cell UI and files

---
+++ Tribes/HabitReportTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  HabitReportTableViewCell.h
+//  Tribes
+//
+//  Created by German Espitia on 3/29/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HabitReportTableViewCell : UITableViewCell
+
+@end
