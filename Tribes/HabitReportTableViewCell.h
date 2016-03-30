4460b43106c591377e47f93bc8dd4327fd728bd2
German Espitia <ge.espitia@gmail.com>
2016-03-30 14:30:31 -0400

    - Add properties to HabitReportTableViewCell - Add image assets for cell ^

--- Tribes/HabitReportTableViewCell.h
+++ Tribes/HabitReportTableViewCell.h
@@ -9,5 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface HabitReportTableViewCell : UITableViewCell
+@property (strong, nonatomic) IBOutlet UILabel *username;
+@property (strong, nonatomic) IBOutlet UILabel *lastWeekCompletions;
+@property (strong, nonatomic) IBOutlet UILabel *thisWeekCompletions;
+@property (strong, nonatomic) IBOutlet UILabel *changeOverWeek;
+@property (strong, nonatomic) IBOutlet UIImageView *thisWeekCompletionsleftView;
+@property (strong, nonatomic) IBOutlet UIImageView *changeLeftView;
 
 @end
