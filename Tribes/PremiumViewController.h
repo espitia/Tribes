cc11a5003dfb2c2c42c2c31bd0195074e56f1e68
German Espitia <ge.espitia@gmail.com>
2016-04-05 12:14:51 -0400

    - Add StoreKit framework - Add premium vc files

---
+++ Tribes/PremiumViewController.h
@@ -0,0 +1,13 @@
+//
+//  PremiumViewController.h
+//  Tribes
+//
+//  Created by German Espitia on 4/5/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PremiumViewController : UIViewController
+
+@end
---
+++ Tribes/PremiumViewController.m
@@ -0,0 +1,37 @@
+//
+//  PremiumViewController.m
+//  Tribes
+//
+//  Created by German Espitia on 4/5/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import "PremiumViewController.h"
+
+@interface PremiumViewController ()
+
+@end
+
+@implementation PremiumViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
