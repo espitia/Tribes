d8abccef8235dd5c3b09b6a6a2c23f287ea84fd4
German Espitia <ge.espitia@gmail.com>
2016-04-07 13:40:39 -0400

    Add PremiumChildVC to hold actual page to be shown for sales. Along with that, functionality to make purchase and restore.

---
+++ Tribes/PremiumChildViewController.h
@@ -0,0 +1,14 @@
+//
+//  PremiumChildViewController.h
+//  Tribes
+//
+//  Created by German Espitia on 4/6/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PremiumChildViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIImageView *backgroundImg;
+@property (assign, nonatomic) NSInteger index;
+@end
