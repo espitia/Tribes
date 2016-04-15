65a3dc38fb957c27e16507d141db65eda512c2a8
German Espitia <germanespitia@germans-mbp>
2016-04-15 14:54:29 -0500

    User IAPHelper model to make purchase simpler (moved logic to model)

--- Tribes/PremiumChildViewController.m
+++ Tribes/PremiumChildViewController.m
@@ -7,136 +7,37 @@
 //
 
 #import "PremiumChildViewController.h"
-#import <QuartzCore/QuartzCore.h>
 #import "IAPHelper.h"
-#import <StoreKit/StoreKit.h>
 
-
-@interface PremiumChildViewController () <SKProductsRequestDelegate, SKPaymentTransactionObserver>
-
-@end
 
 @implementation PremiumChildViewController {
     NSArray * images;
     IBOutlet UIButton *upgradeButton;
+    IAPHelper * iAPHelper;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
    
+    iAPHelper = [[IAPHelper alloc] init];
+    
     [[upgradeButton layer] setBorderWidth:1.0f];
     [[upgradeButton layer] setBorderColor:[UIColor whiteColor].CGColor];
     upgradeButton.layer.cornerRadius = 10; // this value vary as per your desire
     upgradeButton.clipsToBounds = YES;
 }
 
-#pragma mark - Purchasing Premium
-#define kBecomePremiumProductIdentifier @"com.tribes.1monthsub"
-
-
 - (IBAction)upgrade:(id)sender {
-    NSLog(@"User requests to come premium");
-    
-    if([SKPaymentQueue canMakePayments]){
-        NSLog(@"User can make payments");
-        
-        //If you have more than one in-app purchase, and would like
-        //to have the user purchase a different product, simply define
-        //another function and replace kRemoveAdsProductIdentifier with
-        //the identifier for the other product
-        
-        SKProductsRequest *productsRequest = [[SKProductsRequest alloc] initWithProductIdentifiers:[NSSet setWithObject:kBecomePremiumProductIdentifier]];
-        productsRequest.delegate = self;
-        [productsRequest start];
-        
-    }
-    else{
-        NSLog(@"User cannot make payments due to parental controls");
-        //this is called the user cannot make payments, most likely due to parental controls
-    }
+    [iAPHelper make1MonthPremiumPurchase];
 }
 - (IBAction)restore:(id)sender {
-    //this is called when the user restores purchases, you should hook this up to a button
-    [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
-    [[SKPaymentQueue defaultQueue] restoreCompletedTransactions];
+    [iAPHelper restore];
 }
 - (IBAction)close:(id)sender {
     [self dismissViewControllerAnimated:true completion:nil];
 }
 
 
-- (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response{
-    SKProduct *validProduct = nil;
-    NSUInteger count = [response.products count];
-    if(count > 0){
-        validProduct = [response.products objectAtIndex:0];
-        NSLog(@"Products Available!");
-        [self purchase:validProduct];
-    }
-    else if(!validProduct){
-        NSLog(@"No products available");
-        //this is called if your product id is not valid, this shouldn't be called unless that happens.
-    }
-}
 
-- (void)purchase:(SKProduct *)product{
-    SKPayment *payment = [SKPayment paymentWithProduct:product];
-    
-    [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
-    [[SKPaymentQueue defaultQueue] addPayment:payment];
-}
-
-
-- (void) paymentQueueRestoreCompletedTransactionsFinished:(SKPaymentQueue *)queue
-{
-    NSLog(@"received restored transactions: %lu", (unsigned long)queue.transactions.count);
-    for(SKPaymentTransaction *transaction in queue.transactions){
-        if(transaction.transactionState == SKPaymentTransactionStateRestored){
-            //called when the user successfully restores a purchase
-            NSLog(@"Transaction state -> Restored");
-            
-            [self becomePremium];
-            [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
-            break;
-        }
-    }
-}
-
-- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray *)transactions{
-    for(SKPaymentTransaction *transaction in transactions){
-        switch(transaction.transactionState){
-            case SKPaymentTransactionStatePurchasing: NSLog(@"Transaction state -> Purchasing");
-                //called when the user is in the process of purchasing, do not add any of your own code here.
-                break;
-            case SKPaymentTransactionStatePurchased:
-                //this is called when the user has successfully purchased the package (Cha-Ching!)
-                [self becomePremium]; //you can add your code for what you want to happen when the user buys the purchase here, for this tutorial we use removing ads
-                [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
-                NSLog(@"Transaction state -> Purchased");
-                break;
-            case SKPaymentTransactionStateRestored:
-                NSLog(@"Transaction state -> Restored");
-                //add the same code as you did from SKPaymentTransactionStatePurchased here
-                [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
-                break;
-            case SKPaymentTransactionStateFailed:
-                //called when the transaction does not finish
-                if(transaction.error.code == SKErrorPaymentCancelled){
-                    NSLog(@"Transaction state -> Cancelled");
-                    //the user cancelled the payment ;(
-                }
-                [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
-                break;
-            case SKPaymentTransactionStateDeferred:
-                break;
-        }
-    }
-}
-
--(void)becomePremium {
-    IAPHelper * helper = [[IAPHelper alloc] init];
-    [helper make1MonthPremiumPurchase];
-    [self.navigationController dismissViewControllerAnimated:true completion:nil];
-}
 @end
