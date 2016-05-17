622f9360accbe99ed9c3fbaf77842476bf6c06eb
German Espitia <ge.espitia@gmail.com>
2016-05-17 16:41:36 -0400

    Add options to make different purchases from premium vc with an alertview

--- Tribes/PremiumChildViewController.m
+++ Tribes/PremiumChildViewController.m
@@ -8,7 +8,7 @@
 
 #import "PremiumChildViewController.h"
 #import "IAPHelper.h"
-
+#import "SCLAlertView.h"
 
 @implementation PremiumChildViewController {
     NSArray * images;
@@ -21,10 +21,31 @@
     // Do any additional setup after loading the view from its nib.
    
     iAPHelper = [[IAPHelper alloc] init];
+    
+    
+    [[upgradeButton layer] setBorderWidth:1.0f];
+    [[upgradeButton layer] setBorderColor:[UIColor whiteColor].CGColor];
+    [upgradeButton setTitleEdgeInsets:UIEdgeInsetsMake(5.0, 5.0, 5.0,5.0)];
+
+    upgradeButton.layer.cornerRadius = 10; // this value vary as per your desire
+    upgradeButton.clipsToBounds = YES;
 }
 
 - (IBAction)upgrade:(id)sender {
-    [iAPHelper make1MonthPremiumPurchaseWithTableViewController:self andReload:false orDismiss:true];
+
+    
+    SCLAlertView * buyPremium = [[SCLAlertView alloc] initWithNewWindow];
+    [buyPremium addButton:@"Add 1 month for $1.99" actionBlock:^{
+        [iAPHelper makePremiumPurchaseForMonths:1 WithTableViewController:self andReload:false orDismiss:false];
+    }];
+    [buyPremium addButton:@"Add 3 months for $5.99" actionBlock:^{
+        [iAPHelper makePremiumPurchaseForMonths:3 WithTableViewController:self andReload:false orDismiss:false];
+    }];
+    [buyPremium addButton:@"Add 6 months for $9.99" actionBlock:^{
+        [iAPHelper makePremiumPurchaseForMonths:6 WithTableViewController:self andReload:false orDismiss:false];
+    }];
+    [buyPremium showSuccess:@"Upgrade" subTitle:@"Upgrade to Tribes Premium to get access to weekly reports." closeButtonTitle:@"Maybe later" duration:0.0];
+
 }
 - (IBAction)restore:(id)sender {
     [iAPHelper restore];
