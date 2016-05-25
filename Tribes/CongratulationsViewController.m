8ff76babd8432c91f104a5552989058759f5fd5c
German Espitia <ge.espitia@gmail.com>
2016-05-25 12:48:46 -0400

    - add corresponding controller to view - Add confetti rain when user gets to congratulations vc

--- Tribes/CongratulationsViewController.m
+++ Tribes/CongratulationsViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "CongratulationsViewController.h"
+#import "KRConfettiView.h"
 
-@interface CongratulationsViewController ()
+@interface CongratulationsViewController () {
+    KRConfettiView * confettiView;
+}
 
 @end
 
@@ -20,6 +23,10 @@
     
     self.navigationItem.title = @"🎈🎊🎈";
 
+}
+-(void)viewDidAppear:(BOOL)animated {
+    [self makeItRainConfetti];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
@@ -34,5 +41,48 @@
     // Pass the selected object to the new view controller.
 }
 */
+#pragma mark - Confetti
 
+#pragma mark - Confetti
+
+-(void)makeItRainConfetti {
+    
+    // if confetti has not been initialized
+    if (!confettiView) {
+        
+        // Create confetti view
+        confettiView = [[KRConfettiView alloc] initWithFrame:self.view.frame];
+        
+        // Set colors (default colors are red, green and blue)
+        confettiView.colours = @[[UIColor colorWithRed:0.95 green:0.40 blue:0.27 alpha:1.0],
+                                 [UIColor colorWithRed:1.00 green:0.78 blue:0.36 alpha:1.0],
+                                 [UIColor colorWithRed:0.48 green:0.78 blue:0.64 alpha:1.0],
+                                 [UIColor colorWithRed:0.30 green:0.76 blue:0.85 alpha:1.0],
+                                 [UIColor colorWithRed:0.58 green:0.39 blue:0.55 alpha:1.0]];
+        
+        //Set intensity (from 0 - 1, default intensity is 0.5)
+        confettiView.intensity = 0.7;
+        
+        //set type
+        confettiView.type = Diamond;
+        
+        //For custom image
+        //confettiView.customImage = [UIImage imageNamed:@"diamond"];
+        //confettiView.type = Custom;
+    }
+    
+    [NSTimer scheduledTimerWithTimeInterval:1.0f
+                                     target:self
+                                   selector: @selector(stopConfetti)
+                                   userInfo:nil
+                                    repeats:NO];
+    //add subview
+    [self.view addSubview:confettiView];
+    
+    // make it rain confetti!
+    [confettiView startConfetti];
+}
+-(void)stopConfetti {
+    [confettiView stopConfetti];
+}
 @end
