8875b6696a4189efaa3d459906e06c9fa3f8efb2
German Espitia <ge.espitia@gmail.com>
2016-05-26 11:06:06 -0400

    Add LogInVC controller and views

---
+++ Tribes/LogInViewController.m
@@ -0,0 +1,39 @@
+//
+//  LogInViewController.m
+//  Tribes
+//
+//  Created by German Espitia on 5/26/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import "LogInViewController.h"
+
+@interface LogInViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *emailAddress;
+@property (strong, nonatomic) IBOutlet UITextField *password;
+
+@end
+
+@implementation LogInViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)logIn:(id)sender {
+    
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
