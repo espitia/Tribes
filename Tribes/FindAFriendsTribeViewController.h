a563d37d9871ac4cd1991063cc5f2ecf82d9c61d
German Espitia <ge.espitia@gmail.com>
2016-05-27 12:52:04 -0400

    Add FindAFriendsTribe with PFQueryTableVC subclass

---
+++ Tribes/FindAFriendsTribeViewController.h
@@ -0,0 +1,13 @@
+//
+//  FindAFriendsTribeViewController.h
+//  Tribes
+//
+//  Created by German Espitia on 5/27/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import <ParseUI/ParseUI.h>
+
+@interface FindAFriendsTribeViewController : PFQueryTableViewController
+
+@end
