576e12e20fe8b05e3fb82674e2537549e7cd94e1
German Espitia <ge.espitia@gmail.com>
2016-05-27 18:58:37 -0400

    handle joining tribes with corresponding alerts

--- Tribes/FindTribesViewController.m
+++ Tribes/FindTribesViewController.m
@@ -76,6 +76,76 @@
     [self loadObjects];
 }
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    [_searchBar resignFirstResponder];
+    
+    // get tribe
+    Tribe * tribe = [self.objects objectAtIndex:indexPath.row];
+    
+    // ask user to confirm joining tribe
+    NSString * tribeNameAndAdmin = [NSString stringWithFormat:@"%@ by %@", tribe[@"name"], tribe[@"admin"][@"username"]];
+    NSString * alertMessage = [NSString stringWithFormat:@"Just to make sure you didn't tap by accident. Are you sure you want to join %@?", tribeNameAndAdmin];
+    
+    // show alert
+    SCLAlertView * confirmAlert = [[SCLAlertView alloc] initWithNewWindow];
+    [confirmAlert addButton:@"YES" actionBlock:^{
+        
+        // add to tribe
+        [self addToTribe:tribe];
+        
+    }];
+    [confirmAlert addButton:@"NEVER MIND..." actionBlock:^{
+        // show keyboard again
+        [_searchBar becomeFirstResponder];
+    }];
+    [confirmAlert showInfo:@"Confirm" subTitle:alertMessage  closeButtonTitle:nil duration:0.0];
+    
 }
+
+-(void)addToTribe:(Tribe *)tribe {
+    
+    if (tribe.privacy) {
+        // add to private tribe
+        [tribe addUserToTribeOnHold:[User currentUser] withBlock:^(BOOL *success) {
+            if (success) {
+                // pop to root view controller
+                SCLAlertView * successAlert  = [[SCLAlertView alloc] initWithNewWindow];
+                NSString * alertMessage = [NSString stringWithFormat:@"You've successfully been added to %@. Since this is a private Tribe, the admin has been notified to confirm. To make the process quicker, tell him to check his Tribe NOW!",tribe[@"name"]];
+                [successAlert addButton:@"AWESOME" actionBlock:^{
+                    [self.navigationController popToRootViewControllerAnimated:true];
+                }];
+                [successAlert showSuccess:@"Success 😃" subTitle:alertMessage closeButtonTitle:nil duration:0.0];
+            } else {
+                // show error, try again :(
+                SCLAlertView * errorAlert = [[SCLAlertView alloc] initWithNewWindow];
+                [errorAlert showError:@"Oh oh... 😬" subTitle:@"There was an error while adding you to the Tribe. We're sorry. This shouldn't happen. Please check that your internet connection is working and try again." closeButtonTitle:@"GOT IT" duration:0.0];
+            }
+        }];
+        
+        
+        
+    } else {
+        // add to public tribe
+        [tribe addUserToTribe:[User currentUser] withBlock:^(BOOL *success) {
+            
+            if (success) {
+                // pop to root view controller
+                SCLAlertView * successAlert  = [[SCLAlertView alloc] initWithNewWindow];
+                NSString * alertMessage = [NSString stringWithFormat:@"You've successfully been added to %@. Good luck!",tribe[@"name"]];
+                [successAlert addButton:@"AWESOME" actionBlock:^{
+                    [self.navigationController popToRootViewControllerAnimated:true];
+                }];
+                [successAlert showSuccess:@"Success 😃" subTitle:alertMessage closeButtonTitle:nil duration:0.0];
+            } else {
+                // show error, try again :(
+                SCLAlertView * errorAlert = [[SCLAlertView alloc] initWithNewWindow];
+                [errorAlert showError:@"Oh oh... 😬" subTitle:@"There was an error while adding you to the Tribe. We're sorry. This shouldn't happen. Please check that your internet connection is working and try again." closeButtonTitle:@"GOT IT" duration:0.0];
+            }
+            
+            
+        }];
+    }
+}
+
 
 @end
