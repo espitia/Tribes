99f0fe6937c0ff8e57071253302c0eb46d70a726
German Espitia <ge.espitia@gmail.com>
2016-06-06 21:21:57 -0400

    Add adding user to tribe functionality

--- Tribes/AddFriendByUsernameTableViewController.m
+++ Tribes/AddFriendByUsernameTableViewController.m
@@ -72,8 +72,61 @@
     
     [tableView deselectRowAtIndexPath:indexPath animated:true];
     
-
+    User * user = [self.objects objectAtIndex:indexPath.row];
     
+    [_searchBar resignFirstResponder];
+    
+    // show alert to make sure user wants to add selected user to tribe
+    SCLAlertView * confirmAlert = [[SCLAlertView alloc] initWithNewWindow];
+    [confirmAlert addButton:@"YES" actionBlock:^{
+        
+        // make sure user is not already in tribe
+        if (![user.tribes containsObject:_tribe]) {
+            // if user confirm, add user to tribe
+            [_tribe addUserToTribe:user withBlock:^(BOOL *success) {
+                if (success) {
+                    
+                    [_tribe updateTribeWithBlock:^(bool success) {
+                        if (success) {
+                            SCLAlertView * successAlert = [[SCLAlertView alloc] initWithNewWindow];
+                            [successAlert addButton:@"AWESOME" actionBlock:^{
+                                [self.navigationController popToRootViewControllerAnimated:true];
+                            }];
+                            [successAlert showSuccess:@"Success 😄" subTitle:@"You've successfully added your buddy!" closeButtonTitle:nil duration:0.0];
+                            
+                            // send push to newly added member
+                            NSString * message = [NSString stringWithFormat:@"%@ added you to %@!",[User currentUser][@"username"],_tribe[@"name"]];
+                            [[User currentUser] sendPushFromMemberToMember:user withMessage:message habitName:@"" andCategory:@"RELOAD"];
+                        }
+                    }];
+                
+                } else {
+                    SCLAlertView * errorAddingUserAlert = [[SCLAlertView alloc] initWithNewWindow];
+                    [errorAddingUserAlert showError:@"Oh oh.. 😬" subTitle:@"Looks like there was an error adding your friend to the Tribe. Sorry about that. Please try again." closeButtonTitle:@"GOT IT" duration:0.0];
+                }
+            }];
+        }
+        // user already in tribe, inform
+        else {
+            [confirmAlert hideView];
+            SCLAlertView * alreadyInTribeAlert = [[SCLAlertView alloc] initWithNewWindow];
+            [alreadyInTribeAlert addButton:@"GOT IT" actionBlock:^{
+                [_searchBar becomeFirstResponder];
+            }];
+            [alreadyInTribeAlert showError:@"Already in Tribe 🙄" subTitle:@"Looks like your buddy is already in the Tribe." closeButtonTitle:nil duration:0.0];
+        }
+        
+
+        
+    }];
+    
+    NSString * message = [NSString stringWithFormat:@"Just to make sure you are adding the right member. Are you sure you want to add %@ to %@",user[@"username"],_tribe[@"name"]];
+    [confirmAlert addButton:@"NEVER MIND" actionBlock:^{
+        [_searchBar becomeFirstResponder];
+    }];
+    [confirmAlert showInfo:@"CONFIRM" subTitle:message closeButtonTitle:nil duration:0.0];
+    
+
 }
 
 @end
