f1c1a61fa6b1e47ccfc5a94fbb11a6a550dfc017
German Espitia <ge.espitia@gmail.com>
2016-08-12 12:35:00 -0400

    Add ChatViewController extending JSQMessagesVC

---
+++ Tribes/ChatViewController.h
@@ -0,0 +1,14 @@
+//
+//  ChatViewController.h
+//  Tribes
+//
+//  Created by German Espitia on 8/12/16.
+//  Copyright © 2016 German Espitia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "JSQMessagesViewController.h"
+
+@interface ChatViewController : JSQMessagesViewController
+
+@end
